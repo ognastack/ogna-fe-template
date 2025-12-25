fa428ce1eb780ca102edab8dc217e67684bed891
spectrumdevgame-max <ognastack@gmail.com>
2025-12-25 16:18:29 -0500

    more fix routing

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -103,7 +103,10 @@ export function LoginForm({
               </div>
               <div className="mt-4 text-center text-sm">
                 Don&apos;t have an account?{" "}
-                <a href="/auth/signup" className="underline underline-offset-4">
+                <a
+                  href="/account/signup"
+                  className="underline underline-offset-4"
+                >
                   Sign up
                 </a>
               </div>
